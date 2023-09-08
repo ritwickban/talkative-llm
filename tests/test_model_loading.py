2a3b9e259260fe1ff0d0ee535b7b1898051b62dc
zaemyung <zaemyung@gmail.com>
2023-09-08 05:15:09 +0000

    update testcases

--- tests/test_model_loading.py
+++ tests/test_model_loading.py
@@ -1,4 +1,9 @@
+import os
+from glob import glob
 from pathlib import Path
+
+import pytest
+import torch
 
 from talkative_llm.llm import (AlpacaLoraCaller, CohereCaller,
                                HuggingFaceCaller, MPTCaller, OpenAICaller)
@@ -6,14 +11,14 @@ from talkative_llm.llm import (AlpacaLoraCaller, CohereCaller,
 CONFIG_DIR = Path(__file__).parent.parent / 'configs'
 
 
-def test_openai_caller():
-    config_names = ['openai_completion_example.yaml', 'openai_chat_example.yaml']
-    for config_name in config_names:
-        config_path = CONFIG_DIR / 'openai' / config_name
-        caller = OpenAICaller(config=config_path, batch_size=30)
-        assert caller.config['batch_size'] == 30
-        caller.update_caller_params({'temperature': 0.85})
-        assert caller.caller_params['temperature'] == 0.85
+@pytest.mark.parametrize('config_path', glob(os.path.join(CONFIG_DIR, 'openai', '*_example.yaml')))
+def test_openai_caller(config_path: str):
+    print(f'Testing {os.path.basename(config_path)}')
+    caller = OpenAICaller(config=config_path, batch_size=30)
+    assert caller.config['batch_size'] == 30
+    caller.update_caller_params({'temperature': 0.85})
+    assert caller.caller_params['temperature'] == 0.85
+    del caller
 
 
 def test_cohere_caller():
@@ -22,27 +27,38 @@ def test_cohere_caller():
     assert caller.config['batch_size'] == 30
     caller.update_caller_params({'temperature': 0.85})
     assert caller.caller_params['temperature'] == 0.85
+    del caller
 
 
-# def test_huggingface_caller():
-#     config_path = CONFIG_DIR / 'huggingface' / 'huggingface_llm_example.yaml'
-#     caller = HuggingFaceCaller(config=config_path, batch_size=30)
-#     assert caller.config['batch_size'] == 30
-#     caller.update_caller_params({'temperature': 0.85})
-#     assert caller.caller_params['temperature'] == 0.85
+@pytest.mark.parametrize('config_path', glob(os.path.join(CONFIG_DIR, 'huggingface', '*_example.yaml')))
+def test_huggingface_caller(config_path: str):
+    print(f'Testing {os.path.basename(config_path)}')
+    caller = HuggingFaceCaller(config=config_path, batch_size=30)
+    assert caller.config['batch_size'] == 30
+    caller.update_caller_params({'temperature': 0.85})
+    assert caller.caller_params['temperature'] == 0.85
+    del caller
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
 
 
-# def test_mpt_caller():
-#     config_path = CONFIG_DIR / 'mpt' / 'mpt_llm_example.yaml'
-#     caller = MPTCaller(config=config_path, batch_size=5)
-#     assert caller.config['batch_size'] == 5
-#     caller.update_caller_params({'temperature': 0.85})
-#     assert caller.caller_params['temperature'] == 0.85
+def test_mpt_caller():
+    config_path = CONFIG_DIR / 'mpt' / 'mpt_llm_example.yaml'
+    caller = MPTCaller(config=config_path, batch_size=5)
+    assert caller.config['batch_size'] == 5
+    caller.update_caller_params({'temperature': 0.85})
+    assert caller.caller_params['temperature'] == 0.85
+    del caller
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
 
 
-# def test_alpaca_lora_caller():
-#     config_path = CONFIG_DIR / 'alpaca_lora' / 'alpaca_lora_llm_example.yaml'
-#     caller = AlpacaLoraCaller(config=config_path, batch_size=5)
-#     assert caller.config['batch_size'] == 5
-#     caller.update_caller_params({'temperature': 0.85})
-#     assert caller.caller_params['temperature'] == 0.85
+def test_alpaca_lora_caller():
+    config_path = CONFIG_DIR / 'alpaca_lora' / 'alpaca_lora_llm_example.yaml'
+    caller = AlpacaLoraCaller(config=config_path, batch_size=5)
+    assert caller.config['batch_size'] == 5
+    caller.update_caller_params({'temperature': 0.85})
+    assert caller.caller_params['temperature'] == 0.85
+    del caller
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
