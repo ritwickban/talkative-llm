38048b609742e6e74a35d97ee1884abd379447e2
Ritwick Banerjee <baner212@ahl01.agate.msi.umn.edu>
2024-05-29 13:53:41 -0500

    Needs API access. Will take uo after loading HF model

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
-import genai
+import google.generativeai as genai
 model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
 
 response = model.generate_content(["What is the meaning of black hole"])
