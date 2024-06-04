2c0850d2e8f821e6bd3faa1f7f1b1ed251d74263
Ritwick Banerjee <baner212@agc10.agate.msi.umn.edu>
2024-06-04 12:22:31 -0500

    Finally worksgit add .

--- Test2.py
+++ Test2.py
@@ -1,13 +1,15 @@
 import torch
-from accelerate import Accelerator
+from accelerate import Accelerator, load_checkpoint_and_dispatch
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 
-import os
-os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
+#import os
+#os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
 
 # Initialize the Accelerator
 accelerator = Accelerator()
-
+torch.cuda.empty_cache()
 generation_config = GenerationConfig(
     max_new_tokens=64,
     early_stopping=True,
@@ -23,16 +25,35 @@ PROMPTS = [
     'Knock, Knock. Who\'s there?',
     'What makes a great dish?'
 ]
+#if not torch.distributed.is_initialized():
+#    torch.distributed.init_process_group(backend='nccl')
 
 # Load the tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("saltlux/luxia-21.4b-alignment-v1.0")
 model = AutoModelForCausalLM.from_pretrained(
     "saltlux/luxia-21.4b-alignment-v1.0",
     torch_dtype=torch.float16,
+    device_map="auto"
 )
 
+#local_folder='/panfs/jay/groups/29/dongyeop/baner212/NLPInternship/checkpoints/'
+#model.save_pretrained(local_folder)
+#tokenizer.save_pretrained(local_folder)
+
+#model = load_checkpoint_and_dispatch(
+#    model=AutoModelForCausalLM.from_pretrained("saltlux/luxia-21.4b-alignment-v1.0"),
+#    torch_dtype=torch.float16,
+#    device_map="auto"
+#)
+#tokenizer.to(accelerator.device)
+#tokenizer = AutoTokenizer.from_pretrained(local_folder)
+
+#model.to(accelerator.device)
+
+#model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[accelerator.local_process_index])
+
 # Prepare the model and data with the accelerator
-model, tokenizer = accelerator.prepare(model, tokenizer)
+model,tokenizer = accelerator.prepare(model,tokenizer)
 
 # Tokenizing the inputs
 tokenized_inputs = tokenizer(PROMPTS, return_tensors='pt', padding=True).to(accelerator.device)
@@ -41,8 +62,12 @@ tokenized_inputs = tokenizer(PROMPTS, return_tensors='pt', padding=True).to(acce
 with torch.no_grad():
     outputs = model.generate(**tokenized_inputs, generation_config=generation_config)
     decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    all_results=[]
+    for decoded_output in decoded_outputs:
+            all_results.append({'generation': decoded_output})
 
-# Collect the results
-all_results = [{'generation': decoded_output} for decoded_output in decoded_outputs]
+    torch.cuda.empty_cache() #Clearing CUDA Cache
+
+
 
 print(all_results)
